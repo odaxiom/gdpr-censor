273a8043839cf9f32507c49d62b2977af992ed9a
Sébastien Harinck <sebastien.harinck@odaxiom.com>
2020-02-05 22:17:22 +0100

    feat(package): package the code

---
+++ setup.py
@@ -0,0 +1,12 @@
+import sys
+from setuptools import setup, find_packages
+
+
+setup(name='gdpr',
+    version='0.0.1',
+    decription='Aims to convert string data to anonymous data.',
+    author='sebastien.harinck@odaxiom.com',
+    author_email='sebastien.harinck@odaxiom.com',
+    packages=find_packages(),
+    keywords=['gdpr', 'anonymize']
+)

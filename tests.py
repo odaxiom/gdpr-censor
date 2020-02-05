b5a0547072c532acc5dd6b7389b7dbc013594fcc
Sébastien Harinck <sebastien.harinck@odaxiom.com>
2020-02-05 19:57:24 +0100

    tests(gdpr): add first test on gdpr package

---
+++ tests.py
@@ -0,0 +1,21 @@
+import unittest
+import gdpr
+
+
+class GDPRTests(unittest.TestCase):
+    def test_gdpr(self):
+        anonymized = gdpr.anonymize(
+        """
+        Hi ! My name is John Doe and I want to say that I am very glad to be a customer of your store in Paris.
+        Patricia, my advisor helps me a lot to choose my products.
+        I give you my phone number to call me about my joy : 07 86 49 19 63.
+        """
+        )
+
+        self.assertEqual(anonymized,
+        """
+        Hi ! My name is FIRST_NAME LAST_NAME and I want to say that I am very glad to be a customer of your store in Paris.
+        FIRST_NAME, my advisor helps me a lot to choose my products.
+        I give you my phone number to call me about my joy : PHONE_NUMBER.
+        """
+        )
